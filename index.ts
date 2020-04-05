d68dbaadc90801519f3ed401f13a94fe7c2dc702
Simone Bembi <simone.bembi@gmail.com>
2020-04-05 20:13:41 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { Container } from "tsparticles/dist/Classes/Container";
+import { Options } from "tsparticles/dist/Classes/Options/Options";
+import { tsParticles } from "tsparticles/dist/index";
+
+console.log({ Container, Options, tsParticles });
